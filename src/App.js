3f8f54c603ab5488166d9f40b9e784752ad06be0
Jarod <jarodgeorgekolbe@gmail.com>
2022-12-24 12:07:07 +0200

    added log in and signout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Auth.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <p>
+            Hey {user.username}, welcome to my channel, with auth!
+          </p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
